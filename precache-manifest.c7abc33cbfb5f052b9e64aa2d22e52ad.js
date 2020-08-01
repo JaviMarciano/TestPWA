d8c989a2ba5a80c16589dc5d2067bb61d0222650
JavierMarchettiSabra <javiermarchetti83@gmail.com>
2020-08-01 10:32:08 -0300

    initial commit

---
+++ precache-manifest.c7abc33cbfb5f052b9e64aa2d22e52ad.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1509a3d340fe91e72779",
+    "url": "/css/app.74e785b2.css"
+  },
+  {
+    "revision": "c351113696aa2f74eacdf38c411ab20e",
+    "url": "/img/arrow-left.c3511136.svg"
+  },
+  {
+    "revision": "2101e31a49c852ff0c5786630c52d50e",
+    "url": "/img/arrow-right-w.2101e31a.svg"
+  },
+  {
+    "revision": "e96ebdce61152e799e64fef0ae97f10f",
+    "url": "/img/avatar.e96ebdce.png"
+  },
+  {
+    "revision": "f6210e8cfe37f0dfeb017f7b74f31a25",
+    "url": "/img/bg_basquet.f6210e8c.jpg"
+  },
+  {
+    "revision": "d0b7c40ce231ff9463f732258de919d7",
+    "url": "/img/bg_paddle.d0b7c40c.jpg"
+  },
+  {
+    "revision": "340cafbd6d23fc4f3c83eed558aea389",
+    "url": "/img/bg_squash.340cafbd.jpg"
+  },
+  {
+    "revision": "c27d49f19ba63d3287c7161396bc26a5",
+    "url": "/img/bg_tennis.c27d49f1.jpg"
+  },
+  {
+    "revision": "61445b2a8f8ebd830f9f1714a79d8179",
+    "url": "/img/bg_tennis_feed.61445b2a.jpg"
+  },
+  {
+    "revision": "ea8a8af76d47d9ca168ba84331b67c95",
+    "url": "/img/icon-eye-slash.ea8a8af7.svg"
+  },
+  {
+    "revision": "c9be2fc9b62db3d65862fc2a673d1374",
+    "url": "/img/icon-eye.c9be2fc9.svg"
+  },
+  {
+    "revision": "dc39268ab57cc125ac17a352de317efd",
+    "url": "/img/icon-facebook.dc39268a.svg"
+  },
+  {
+    "revision": "8e87a864b9832cb6803cf363aea415fc",
+    "url": "/img/icon-gear.8e87a864.svg"
+  },
+  {
+    "revision": "fa0a834dfb39f21ece93aa6e8118a066",
+    "url": "/img/icon-gps.fa0a834d.svg"
+  },
+  {
+    "revision": "7805f57e491b4a9401b87d20a06e0578",
+    "url": "/img/icon-linkedin.7805f57e.svg"
+  },
+  {
+    "revision": "05f6de0d2ccaaaf2bb273a99be63255c",
+    "url": "/img/icon-lock.05f6de0d.svg"
+  },
+  {
+    "revision": "985896cac24a4deaa6396a98c0699368",
+    "url": "/img/icon-search.985896ca.svg"
+  },
+  {
+    "revision": "9271e0983e144db9a9271ffbd1bc493e",
+    "url": "/img/icon-trophy.9271e098.svg"
+  },
+  {
+    "revision": "8924d20b648e7b9e1e0411c740f577fc",
+    "url": "/img/icon-twitter.8924d20b.svg"
+  },
+  {
+    "revision": "0aa613624f9e90ac035eda36eecb4a46",
+    "url": "/img/icon-user.0aa61362.svg"
+  },
+  {
+    "revision": "7ed97511356f8e8e1751e0d06fb3edd2",
+    "url": "/img/ribbon.7ed97511.svg"
+  },
+  {
+    "revision": "bd85275717bbb5f9e735c9f70f2ab7e3",
+    "url": "/img/splash.bd852757.png"
+  },
+  {
+    "revision": "d079df4d28fe549371361b3015c375da",
+    "url": "/img/spotzu_bc.d079df4d.svg"
+  },
+  {
+    "revision": "57f29ed75b79820d6b75f3fb1d27c28d",
+    "url": "/img/spotzu_wc.57f29ed7.svg"
+  },
+  {
+    "revision": "c09e198c69119c3b5e5ef3bead8d3372",
+    "url": "/img/top-profile.c09e198c.svg"
+  },
+  {
+    "revision": "fcd7c70c75973cbeead188690db84505",
+    "url": "/img/toplogin.fcd7c70c.svg"
+  },
+  {
+    "revision": "e40620cb4d98a1fef437801ca306b9b0",
+    "url": "/img/v700.e40620cb.jpg"
+  },
+  {
+    "revision": "14fccb87dcdf118d39ca8d12d5116f93",
+    "url": "/img/vs.14fccb87.svg"
+  },
+  {
+    "revision": "7493020c7732d84077ea8b7ad34f9c83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ec1fcdbbb5dd602721a",
+    "url": "/js/about.e6d6103b.js"
+  },
+  {
+    "revision": "1509a3d340fe91e72779",
+    "url": "/js/app.41088ac8.js"
+  },
+  {
+    "revision": "848ee7338c25eae86568",
+    "url": "/js/chunk-vendors.353b30b3.js"
+  },
+  {
+    "revision": "772fa2f0ab2bbce6437083269c46993f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
